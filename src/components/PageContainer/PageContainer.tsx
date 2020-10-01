3257b873519d0adf9b173fe80c3544643472a152
Weverton Rodrigues <wevertonfr@discente.ufg.br>
2020-10-01 14:43:34 -0300

    botar um emoji com meu nome né pae 🧡

--- src/components/PageContainer/PageContainer.tsx
+++ src/components/PageContainer/PageContainer.tsx
@@ -10,7 +10,15 @@ const PageContainer: React.FC<any> = ({ children, title }) => {
         <title>{title}</title>
       </Helmet>
       <div className="page-container">
-        <div className="page-wrapper">{children}</div>
+        <div className="page-wrapper">
+          {children}
+          <small>
+            Feito com 🧡 por{" "}
+            <a href="https://weverton.me" target="_blank">
+              Weverton
+            </a>
+          </small>
+        </div>
       </div>
     </>
   );
